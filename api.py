a32eca19f711f8d5db5cf3dee3020061c3666c4f
himanshu17-2000 <earthboxer@gmail.com>
2023-08-18 11:41:35 +0530

    new backend working fine

--- api.py
+++ api.py
@@ -32,7 +32,6 @@ def get_users():
 def get_transaction():
     try:
         data = Transactions.query.all() 
-        print(type(data[0].timestamp))
         def get_dict(item):
             return {
                 "from" : item.from_user,
@@ -47,14 +46,14 @@ def get_transaction():
 @api.route('/register' , methods =['POST'])  
 def register_user():
     try:
-       data = request.json
-       existing_user =  Users.query.filter(Users.email == data['email']).first()
-       if(existing_user != None):
-            return json.dumps({'message':"email already exists"}) ,400 
-       new_user = Users(id =random.randint(10000000 ,99999999), name = data['name'], email= data['email'],phone= data['phone'],amount= data['amount'])
-       db.session.add(new_user)
-       db.session.commit()
-       return json.dumps({"message" : "user registered" , 'id':new_user.id}),200 
+        data = request.json
+        existing_user =  Users.query.filter(Users.email == data['email']).first()
+        if(existing_user != None):
+                return json.dumps({'message':"email already exists"}) ,400 
+        new_user = Users(id =random.randint(10000000 ,99999999), name = data['name'], email= data['email'],phone= data['phone'],amount= data['amount'])
+        db.session.add(new_user)
+        db.session.commit()
+        return json.dumps({"message" : "user registered" , 'id':new_user.id}),200 
     except Exception as err:
             return json.dumps( { "message" :f"error occur {err}"}) , 400 
 
@@ -62,20 +61,20 @@ def register_user():
 @api.route('/transfer' , methods = ['POST'])
 def transfer():
     try:
-      data = request.json 
-      from_user = Users.query.filter(Users.id == data['from_account']).first()
-      old_from_amt = from_user.amount 
-      from_user.amount = old_from_amt - data['amount']
-      to_user = Users.query.filter(Users.id == data['to_account']).first()
-      old_to_amt = to_user.amount 
-      to_user.amount = old_to_amt + data['amount']
-      new_transaction = Transactions(from_user = from_user.name , to_user = to_user.name , amount = data['amount'] )
-      db.session.add(new_transaction)
-      db.session.commit()
-      return json.dumps({"message":"transaction saved sucessfully"}),200
+        data = request.json 
+        from_user = Users.query.filter(Users.id == data['from_account']).first()
+        old_from_amt = from_user.amount 
+        from_user.amount = old_from_amt - data['amount']
+        to_user = Users.query.filter(Users.id == data['to_account']).first()
+        old_to_amt = to_user.amount 
+        to_user.amount = old_to_amt + data['amount']
+        new_transaction = Transactions(from_user = from_user.name , to_user = to_user.name , amount = data['amount'] )
+        db.session.add(new_transaction)
+        db.session.commit()
+        return json.dumps({"message":"transaction saved sucessfully"}),200
     except Exception as err:
                 return json.dumps( { "message" :f"error occur {err}"}) , 400 
-      
+    
 
 @api.route("/user/<int:id>" , methods =['GET'])
 def get_customer_by_id(id):
